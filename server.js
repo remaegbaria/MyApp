8258e801e631a47de9a3d2f466c5c971b6c0094b
rema <remawail06@gmail.com>
2021-01-24 19:04:12 +0200

    x

--- server.js
+++ server.js
@@ -6,57 +6,57 @@ var bodyParser = require('body-parser')
 app.use(express.static('rct/build'));
 app.use(bodyParser.json())
 
-const images = [
-    { id:234, url: 'https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGF3bnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',score:0 }
-  ]
+// const images = [
+//     { id:234, url: 'https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGF3bnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',score:0 }
+//   ]
 
 
-  function uniqueId(){
-    return "id" + Math.random().toString(16).slice(2)
-  }
+//   function uniqueId(){
+//     return "id" + Math.random().toString(16).slice(2)
+//   }
 
 //routes
-app.get('/get-images', (req, res) => {
+// app.get('/get-images', (req, res) => {
 
-  res.send(images)
-})
+//   res.send(images)
+// })
 
-function compare( a, b ) {
-    if ( a.score < b.score ){
-      return 1;
-    }
-    if ( a.score > b.score ){
-      return -1;
-    }
-    return 0;
-  }
+// function compare( a, b ) {
+//     if ( a.score < b.score ){
+//       return 1;
+//     }
+//     if ( a.score > b.score ){
+//       return -1;
+//     }
+//     return 0;
+//   }
 
-app.get('/get-sorted-images', (req, res) => {
-    let sortedImages = images.slice();
-    sortedImages = sortedImages.sort(compare);
-    res.send(sortedImages);
-  })
+// app.get('/get-sorted-images', (req, res) => {
+//     let sortedImages = images.slice();
+//     sortedImages = sortedImages.sort(compare);
+//     res.send(sortedImages);
+//   })
 
-app.post('/addImg', (req, res) => {
-    const url= req.body;
-    let id = uniqueId();
-    let score =0;
-    const index = images.findIndex(img => img.id === id);
-    if (index < 0 ) {
-    images.push({id:id,url:url.img,score:score});
-    }
-})
+// app.post('/addImg', (req, res) => {
+//     const url= req.body;
+//     let id = uniqueId();
+//     let score =0;
+//     const index = images.findIndex(img => img.id === id);
+//     if (index < 0 ) {
+//     images.push({id:id,url:url.img,score:score});
+//     }
+// })
 
-app.post('/ImagesAfterRating', (req, res) => {
+// app.post('/ImagesAfterRating', (req, res) => {
 
-    const imagesToUpdate = req.body.Copyimages;
-    imagesToUpdate.forEach(element => {
-        const index = images.findIndex(img => img.id === element.id);
-        if(index > -1)
-        images[index].score = element.score;
-    });
+//     const imagesToUpdate = req.body.Copyimages;
+//     imagesToUpdate.forEach(element => {
+//         const index = images.findIndex(img => img.id === element.id);
+//         if(index > -1)
+//         images[index].score = element.score;
+//     });
 
-})
+// })
 
 const port = process.env.PORT || 3002;
 
